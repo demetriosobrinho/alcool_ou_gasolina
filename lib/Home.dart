292aeb2443b1b3841e587ab926193333ef2278ad
demetriosobrinho <demetriosobrinho@gmail.com>
2021-11-18 16:56:30 -0300

    second commit

---
+++ lib/Home.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  TextEditingController _controllerAlcool = TextEditingController();
+  TextEditingController _controllerGasolina = TextEditingController();
+  String _textoResultado = "";
+
+
+  void _calcular(){
+
+    double precoAlcool = double.tryParse( _controllerAlcool.text.replaceAll(",", ".") ) ?? 0.0;
+    double precoGasolina = double.tryParse( _controllerGasolina.text.replaceAll(",", ".") ) ?? 0.0;
+
+    print("Alcool: " + precoAlcool.toString());
+    print("Gasolina: " + precoGasolina.toString());
+
+     if( precoAlcool == 0.0 || precoGasolina == 0.0 ){
+       setState(() {
+         _textoResultado = "Número inválido, digite números maiores que 0";
+       });
+     }else{
+
+       /*
+       * Se o preço do álcool divido pelo preço da gasolina
+       * for >= a 0.7 é melhor abastecer com gasolina
+       * senão é melhor utilizar álcool
+       * */
+       if( (precoAlcool / precoGasolina) >= 0.7 ){
+         setState(() {
+           _textoResultado = "Melhor abastecer com gasolina";
+         });
+       }else{
+         setState(() {
+           _textoResultado = "Melhor abastecer com alcool";
+         });
+       }
+
+       _limparCampos();
+
+     }
+
+
+  }
+
+   void _limparCampos(){
+     _controllerGasolina.text = "";
+     _controllerAlcool.text = "";
+   }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Álcool ou Gasolina"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
+                child: Image.asset("images/logo.png"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Saiba qual a melhor opção para abastecimento do seu carro",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Alcool, ex: 1.59"
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerAlcool,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Gasolina, ex: 3.59"
+                ),
+                style: TextStyle(
+                    fontSize: 22
+                ),
+                controller: _controllerGasolina,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: RaisedButton(
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(
+                          fontSize: 20
+                      ),
+                    ),
+                    onPressed: _calcular
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  _textoResultado,
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
